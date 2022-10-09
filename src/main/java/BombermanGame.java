b820feb3bca6f7b900df811d95d1b5b418a085c7
mairyy <21020125@vnu.edu.vn>
2022-10-09 17:29:18 +0700

    sửa main

--- src/main/java/BombermanGame.java
+++ src/main/java/BombermanGame.java
@@ -4,9 +4,21 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+// Status
+enum STATUS {
+    START, HELPMENU, HIGHSCORE, SETTINGS
+}
+
 public class BombermanGame extends Application {
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 800;
+    public static Group root = new Group();
+    public static STATUS status;
+
     public static void main(String[] agrs) {
         Application.launch(BombermanGame.class);
     }
@@ -15,13 +27,25 @@ public class BombermanGame extends Application {
     public void start(Stage theStage) {
         theStage.setTitle("Bomberman");
 
-        Group root = new Group();
         Scene theScene = new Scene(root);
         theStage.setScene(theScene);
 
-        Canvas canvas = new Canvas(400, 400);
+        Canvas canvas = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
         root.getChildren().add(canvas);
 
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        clearScreen(gc);
+
+        Menu m = new Menu();
+        m.loadButton(gc);
+        m.renderButton(gc);
+
         theStage.show();
+    }
+
+    public static void clearScreen(GraphicsContext gc) {
+        Color color = Color.rgb(0, 255, 0);
+        gc.setFill(color);
+        gc.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
     }
 }
