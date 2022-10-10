db9ce1424f6abf390ca0e96a7a1a63a2e7492f55
nguyenvankhang <21020768@vnu.edu.vn>
2022-10-10 09:07:31 +0700

    update Main

--- src/uet/oop/Main.java
+++ src/uet/oop/Main.java
@@ -1,17 +1,27 @@
-package main.java;
+package uet.oop;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
+import java.lang.annotation.Retention;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
-    final static int widthUnit =  25;
+    public final static int widthUnit =  50;
+    public static int widthScreen;
+    public static int heightScreen;
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,15 +32,47 @@ public class Main extends Application {
         Group root = new Group();
         Scene scene = new Scene(root);
         stage.setScene(scene);
-
-        Canvas canvas = new Canvas(25*9,25*9);
-        root.getChildren().add(canvas);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        File file = new File("src/resource/map/map1.txt");
+        File file = new File("src/main/resource/map/map1.txt");
         Map map = new Map();
         map.loadMap(file);
+        widthScreen = map.getLenWidth()*widthUnit;
+        heightScreen = map.getLenHeight()*widthUnit;
+        Canvas canvas = new Canvas(widthScreen, heightScreen);
+        Rectangle2D abc = new Rectangle();
+        root.getChildren().add(canvas);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //handle event.
+        List<KeyCode> events = new ArrayList<>();
+        scene.setOnKeyPressed(
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent keyEvent) {
+                        if(!events.contains(keyEvent.getCode())) {
+                            events.add(keyEvent.getCode());
+                        }
+                    }
+                }
+        );
+        scene.setOnKeyReleased(
+                new EventHandler<KeyEvent>() {
+                    @Override
+                    public void handle(KeyEvent keyEvent) {
+                        events.remove(keyEvent.getCode());
+                    }
+                }
+        );
+        new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                map.handleInput(events);
+                map.render(gc);
+            }
+        }.start();
+
         map.render(gc);
 
         stage.show();
     }
+
 }
