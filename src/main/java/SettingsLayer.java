c55e1392c86f7d728dafebfef6d4d0306e26407e
mairyy <21020125@vnu.edu.vn>
2022-10-18 10:24:26 +0700

    thêm ảnh và tạo class SettingsLayer để tùy chỉnh tắt/bật nhạc

---
+++ src/main/java/SettingsLayer.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class SettingsLayer {
+    public static void renderButton() {
+        BombermanGame.root.getChildren().remove(Menu.helpButton.circle);
+        BombermanGame.root.getChildren().remove(Menu.settingButton.circle);
+        BombermanGame.root.getChildren().remove(Menu.highScoreButton.circle);
+        BombermanGame.root.getChildren().remove(Menu.startButton.circle);
+        BombermanGame.root.getChildren().add(Menu.musicButton.circle);
+        BombermanGame.root.getChildren().add(Menu.soundButton.circle);
+        BombermanGame.root.getChildren().add(Menu.backButton.circle);
+    }
+}
